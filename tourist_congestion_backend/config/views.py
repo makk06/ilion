90217dcb210f76676c024c08dd26a0486300a1ac
hurdooagent <301966593+hurdooagent@users.noreply.github.com>
2026-09-20 22:23:27 +0900

    feat: VWorld 클라이언트 설정 API 추가

--- tourist_congestion_backend/config/views.py
+++ tourist_congestion_backend/config/views.py
@@ -1,8 +1,32 @@
-from django.http import JsonResponse
-from django.views.decorators.http import require_GET
-from django.conf import settings
-from django.db import connection, DatabaseError
+import os
 import time
+
+from django.conf import settings
+from django.db import DatabaseError, connection
+from django.http import JsonResponse
+from django.views.decorators.debug import sensitive_variables
+from django.views.decorators.http import require_GET
+
+
+@require_GET
+@sensitive_variables('vworld_api_key')
+def client_config(request):
+    """Return public runtime settings needed to initialize the client."""
+    vworld_api_key = os.environ.get('VWORLD_API_KEY', '').strip()
+    if not vworld_api_key:
+        response = JsonResponse({
+            'success': False,
+            'data': None,
+            'message': '지도 설정을 일시적으로 불러올 수 없습니다.',
+        }, status=503)
+    else:
+        response = JsonResponse({
+            'success': True,
+            'data': {'vworld_api_key': vworld_api_key},
+            'message': '',
+        })
+    response['Cache-Control'] = 'no-store'
+    return response
 
 
 @require_GET
